ccf8690d3601b9664d4fd8c52149a0458d1f2f43
Mahatmaditya FRS <mahatmadityafrs88@gmail.com>
2023-08-17 20:45:51 +0700

    create SignInWithApple view

---
+++ Halo Vets/SignInWithApple.swift
@@ -0,0 +1,20 @@
+//
+//  SignInWithApple.swift
+//  Halo Vets
+//
+//  Created by Mahatmaditya FRS on 17/08/23.
+//
+
+import SwiftUI
+
+struct SignInWithApple: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInWithApple_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInWithApple()
+    }
+}

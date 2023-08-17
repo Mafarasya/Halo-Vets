e2879c6bad382feb7d4cc9875aca77b388b8d000
Mahatmaditya FRS <mahatmadityafrs88@gmail.com>
2023-08-17 20:32:45 +0700

    Initial Commit

---
+++ Halo Vets/Halo_VetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Halo_VetsApp.swift
+//  Halo Vets
+//
+//  Created by Mahatmaditya FRS on 17/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Halo_VetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

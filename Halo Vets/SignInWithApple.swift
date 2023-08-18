c4ccde01a5fbd5bb5073e1e7d8b033a0c681b244
Mahatmaditya FRS <mahatmadityafrs88@gmail.com>
2023-08-18 18:30:51 +0700

    - Update Sign In With Apple and Test to show first name and last name from user

--- Halo Vets/SignInWithApple.swift
+++ Halo Vets/SignInWithApple.swift
@@ -9,6 +9,13 @@ import AuthenticationServices
 import SwiftUI
 
 struct SignInWithApple: View {
+    
+    
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userId") var userId: String = ""
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -17,42 +24,70 @@ struct SignInWithApple: View {
             
             // MARK: Sign In with Apple ID
             VStack {
-                // MARK: handle the request
-                SignInWithAppleButton(.continue) { request in
-                    
-                    request.requestedScopes = [.email, .fullName]
-                    
-                // MARK: handle the result
-                } onCompletion: { result in
-                    
-                    switch result {
-                    case .success(let auth):
-                        
-                        switch auth.credential {
-                        case let appleCredential as ASAuthorizationAppleIDCredential:
-                            
-                            // get information about the user - should be cache i think
-                            // User ID
-                            let userId = appleCredential.user
-                            
-                            // User Info
-                            let email = appleCredential.email
-                            let firstName = appleCredential.fullName?.givenName
-                            let lastName = appleCredential.fullName?.familyName
-                            
-                        default:
-                            break
-                        }
-                        
-                    case .failure(let error):
-                        
-                        print(error)
-                        
-                    }
-                    
+                
+                if userId.isEmpty {
+                    SignInButtonView()
                 }
+                else {
+                    // Signed In
+                    Text("welcome back! \(firstName) \(lastName)")
+                }
+                
+                
             }
-            .navigationTitle("Sign In")
+            
+        }
+        
+    }
+}
+
+struct SignInButtonView: View {
+    
+    @AppStorage("email") var email: String = ""
+    @AppStorage("firstName") var firstName: String = ""
+    @AppStorage("lastName") var lastName: String = ""
+    @AppStorage("userId") var userId: String = ""
+    
+    var body: some View {
+        // MARK: handle the request
+        SignInWithAppleButton(.continue) { request in
+            
+            request.requestedScopes = [.email, .fullName]
+            
+            // MARK: handle the result
+        } onCompletion: { result in
+            
+            switch result {
+            case .success(let auth):
+                
+                switch auth.credential {
+                case let appleCredential as ASAuthorizationAppleIDCredential:
+                    
+                    // get information about the user - should be cache i think
+                    // User ID
+                    let userId = appleCredential.user
+                    
+                    // User Info
+                    let email = appleCredential.email
+                    let firstName = appleCredential.fullName?.givenName
+                    let lastName = appleCredential.fullName?.familyName
+                    
+                    self.email = email ?? ""
+                    self.userId = userId
+                    self.firstName = firstName ?? ""
+                    self.lastName = lastName ?? ""
+                    
+                    
+                default:
+                    break
+                }
+                
+            case .failure(let error):
+                
+                print(error)
+                
+            }
+            
         }
     }
 }

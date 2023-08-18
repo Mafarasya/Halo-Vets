6a7b166bfe0c7ea510b55161e0fcd30e3dbd9f91
Mahatmaditya FRS <mahatmadityafrs88@gmail.com>
2023-08-18 09:21:19 +0700

    - Adding sign in with apple id fiture

--- Halo Vets/SignInWithApple.swift
+++ Halo Vets/SignInWithApple.swift
@@ -5,11 +5,55 @@
 //  Created by Mahatmaditya FRS on 17/08/23.
 //
 
+import AuthenticationServices
 import SwiftUI
 
 struct SignInWithApple: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                
+            }
+            
+            // MARK: Sign In with Apple ID
+            VStack {
+                // MARK: handle the request
+                SignInWithAppleButton(.continue) { request in
+                    
+                    request.requestedScopes = [.email, .fullName]
+                    
+                // MARK: handle the result
+                } onCompletion: { result in
+                    
+                    switch result {
+                    case .success(let auth):
+                        
+                        switch auth.credential {
+                        case let appleCredential as ASAuthorizationAppleIDCredential:
+                            
+                            // get information about the user - should be cache i think
+                            // User ID
+                            let userId = appleCredential.user
+                            
+                            // User Info
+                            let email = appleCredential.email
+                            let firstName = appleCredential.fullName?.givenName
+                            let lastName = appleCredential.fullName?.familyName
+                            
+                        default:
+                            break
+                        }
+                        
+                    case .failure(let error):
+                        
+                        print(error)
+                        
+                    }
+                    
+                }
+            }
+            .navigationTitle("Sign In")
+        }
     }
 }
 
